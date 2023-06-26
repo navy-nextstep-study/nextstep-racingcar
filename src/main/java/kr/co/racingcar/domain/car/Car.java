df7a215f8ec46db04a2874068cf4615671f40728
HyuckJuneHong <jjj8337721@naver.com>
2023-06-26 22:13:11 +0900

    feat : 자동차 이름이 빈 문자열, 공백, null인지 검증하는 기능 구현

--- src/main/java/kr/co/racingcar/domain/car/Car.java
+++ src/main/java/kr/co/racingcar/domain/car/Car.java
@@ -6,4 +6,16 @@ public class Car {
             throw new IllegalArgumentException("자동차 이름은 5글자를 넘을 수 없습니다.");
         }
     }
+
+    public void validateCarNameBlank(String input) {
+        if(input.isBlank()){
+            throw new IllegalArgumentException("자동차 이름이 빈문자열이거나 공백입니다.");
+        }
+    }
+
+    public void validateCarNameNull(String input) {
+        if(input == null){
+            throw new IllegalArgumentException("자동차 이름이 Null 입니다.");
+        }
+    }
 }
--- src/test/java/kr/co/racingcar/domain/car/CarTest.java
+++ src/test/java/kr/co/racingcar/domain/car/CarTest.java
@@ -1,6 +1,7 @@
 package kr.co.racingcar.domain.car;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -22,5 +23,29 @@ class CarTest {
         assertThatThrownBy(() -> car.validateCarNameLength(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 5글자를 넘을 수 없습니다.");
+    }
+
+    @Test
+    void validateCarNameBlank_자동차_이름이_빈문자열_IllegalArgumentException() {
+        //given & when & then
+        assertThatThrownBy(() -> car.validateCarNameBlank(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름이 빈문자열이거나 공백입니다.");
+    }
+
+    @Test
+    void validateCarNameBlank_자동차_이름이_공백으로_이루어짐_IllegalArgumentException() {
+        //given & when & then
+        assertThatThrownBy(() -> car.validateCarNameBlank("       "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름이 빈문자열이거나 공백입니다.");
+    }
+
+    @Test
+    void validateCarNameNull_자동차_이름이_null_IllegalArgumentException() {
+        //given & when & then
+        assertThatThrownBy(() -> car.validateCarNameNull(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름이 Null 입니다.");
     }
 }
