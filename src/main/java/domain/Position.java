22661ba1966ca1425e8fd8763c1921b76405ca2a
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-29 03:06:27 +0900

    feat : position이 이동되는 기능을 추가

---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Position {
+
+    private int position;
+
+    public void addPosition() {
+        position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
