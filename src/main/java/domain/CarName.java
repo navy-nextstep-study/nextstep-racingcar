1612d623474fed6ae24e75e64e514c6f3a8d8216
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-29 11:32:07 +0900

    refactor : CarName의 매직 넘버를 제거하였습니다.

--- src/main/java/domain/CarName.java
+++ src/main/java/domain/CarName.java
@@ -2,7 +2,8 @@ package domain;
 
 public class CarName {
 
-    private static final String NAME_LENGTH_ERROR_MSG = "자동차의 이름은 5자 이내여야 합니다.";
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String NAME_LENGTH_ERROR_MSG = "자동차의 이름은 " + MAXIMUM_NAME_LENGTH + "자 이내여야 합니다.";
     private final String name;
     public CarName(String name) {
         validNameLength(name);
@@ -10,7 +11,7 @@ public class CarName {
     }
 
     public void validNameLength(String name) {
-        if(name.length() > 5) {
+        if(name.length() > MAXIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(NAME_LENGTH_ERROR_MSG);
         }
     }
